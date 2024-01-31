53a40b9ac6f5f9e6eeabee2ba7ffc8644ba5833a
Maxim <126081409+Maxim-Levchenko@users.noreply.github.com>
2024-01-31 22:34:30 +0200

    prepared-main

---
+++ static/source/js/main.js
@@ -0,0 +1,27 @@
+
+var curr_time = new Date().getHours();
+var greeting_section = document.getElementById("greetingHeading");
+
+if (curr_time >= 5 && curr_time < 12) {
+    greeting_section.innerHTML = "Good morning";
+} else if (curr_time >= 12 && curr_time < 18) {
+    greeting_section.innerHTML = "Good afternoon";
+} else {
+    greeting_section.innerHTML = "Good evening";
+}
+document.addEventListener('DOMContentLoaded', function() {
+    var scroll = window.innerHeight < document.body.scrollHeight;
+    var footer = document.querySelector('footer');
+    if (scroll) {
+        footer.classList.remove('fixed-bottom');
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var scroll = window.innerHeight < document.body.scrollHeight;
+    var footer = document.querySelector('footer');
+    if (scroll) {
+        footer.classList.remove('fixed-bottom');
+    }
+});
